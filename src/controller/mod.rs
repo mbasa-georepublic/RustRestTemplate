c31d2a4d5f2495f18abbde417812f29ab3a1c38c
Mario Basa <mario.basa@gmail.com>
2020-12-18 16:26:07 +0900

    ran rustfmt

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,5 +1,5 @@
-use rocket_contrib::json::Json;
 use crate::models::employee;
+use rocket_contrib::json::Json;
 
 #[get("/")]
 pub fn index() -> &'static str {
@@ -7,28 +7,37 @@ pub fn index() -> &'static str {
 }
 
 #[get("/hello/<name>/<age>")]
-pub fn hello(name:String,age:u8) -> String {
+pub fn hello(name: String, age: u8) -> String {
     let mage = age + 2;
     format!("Hello, {} year old named {}!", mage, name)
 }
 
 #[get("/employee")]
 pub fn employee() -> Json<employee::Employee> {
-    let skill1 = employee::Skill{id:1,desc:String::from("programmer") };
-    let skill2 = employee::Skill{id:2,desc:String::from("designer") };
-    let skill3 = employee::Skill{id:3,desc:String::from("debugger") };        
-    
-    let mut skills:Vec<employee::Skill> = Vec::new();
+    let skill1 = employee::Skill {
+        id: 1,
+        desc: String::from("programmer"),
+    };
+    let skill2 = employee::Skill {
+        id: 2,
+        desc: String::from("designer"),
+    };
+    let skill3 = employee::Skill {
+        id: 3,
+        desc: String::from("debugger"),
+    };
+
+    let mut skills: Vec<employee::Skill> = Vec::new();
     skills.push(skill1);
     skills.push(skill2);
     skills.push(skill3);
-            
-    let _emp = employee::Employee{
-        name:String::from("Mario Basa"),
+
+    let _emp = employee::Employee {
+        name: String::from("Mario Basa"),
         age: 42,
         dept: String::from("Production"),
-        skills: skills
+        skills: skills,
     };
-        
+
     Json(_emp)
 }
--- src/models/employee.rs
+++ src/models/employee.rs
@@ -1,17 +1,17 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Employee{
-    pub name  : String,
-    pub age   : u16,
-    pub dept  : String,
-    pub skills: Vec<Skill>//[Skill;3]
+pub struct Employee {
+    pub name: String,
+    pub age: u16,
+    pub dept: String,
+    pub skills: Vec<Skill>, //[Skill;3]
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Skill {
     pub id: u16,
-    pub desc: String
+    pub desc: String,
 }
 /*
 impl Skill {
